1846618fda1599df321ae682fdd017836249a498
Shayla White <shayladwhite1@gmail.com>
2019-05-07 03:22:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(phrase=)
+def speak_to_grandma(phrase="I LOVE YOU GRANDMA")
    if phrase == "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN!"  
   else if phrase == "I LOVE YOU GRANDMA".upcase
