4c1fcca8d9d2c5e05e9336ca5a8f5bb15593ad7f
Shayla White <shayladwhite1@gmail.com>
2019-05-07 03:17:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,9 @@
 
 def speak_to_grandma(phrase)
    if phrase == "I LOVE YOU GRANDMA"
-    
+    return "I LOVE YOU TOO PUMPKIN!"  
+  else if phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
  end
 
